9c60d7f06d6c923b542999c01bf5c43ff815da8c
Kenan02 <61836371+Kenan02@users.noreply.github.com>
2021-04-30 17:53:47 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 basic.showString("Hello!")
+OLED.init(128, 64)
 basic.forever(function () {
     if (tinkercademy.PIR(DigitalPin.P0)) {
         tinkercademy.LED(DigitalPin.P1, OnOff.On)
         music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showString("FARA")
+        OLED.writeStringNewLine("FARA")
     } else {
         tinkercademy.LED(DigitalPin.P1, OnOff.Off)
     }
